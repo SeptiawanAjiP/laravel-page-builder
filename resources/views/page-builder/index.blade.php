886cd623ca2e1601004cf5032c194ab7ae726998
septiawanajipradana <septiawanajipradana@gmail.com>
2023-12-28 22:23:34 +0700

    feat: change welcome page with bootstrap

---
+++ resources/views/page-builder/index.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>GrapesJS Preset Webpage</title>
+    <link href="https://unpkg.com/grapesjs/dist/css/grapes.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/grapesjs"></script>
+    <script src="https://unpkg.com/grapesjs-preset-webpage"></script>
+    <script src="https://unpkg.com/grapesjs-preset-newsletter"></script>
+    <script src="https://unpkg.com/grapesjs-navbar"></script>
+    <script src="https://unpkg.com/grapesjs-plugin-forms"></script>
+    <script src="https://unpkg.com/grapesjs-image-plugin"></script>
+    <style>
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id="gjs" style="height:0px; overflow:hidden">
+
+    </div>
+
+
+    <script type="text/javascript">
+        window.onload = () => {
+            window.editor = grapesjs.init({
+                height: '100%',
+                showOffsets: true,
+                noticeOnUnload: false,
+                storageManager: false,
+                container: '#gjs',
+                fromElement: true,
+
+                plugins: [
+                    'grapesjs-preset-webpage',
+                    'grapesjs-preset-newsletter',
+                    'grapesjs-navbar',
+                    'grapesjs-plugin-forms',
+                    'grapesjs-image-plugin'
+                ],
+                pluginsOpts: {
+                    'grapesjs-image-plugin': {}
+                }
+
+            });
+        };
+    </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/page-builder', function () {
+    return view('page-builder.index');
+});
