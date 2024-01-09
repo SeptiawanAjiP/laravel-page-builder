4dd48e3f24371e1037df0e864841dfdfa7aca1ba
septiawanajipradana <septiawanajipradana@gmail.com>
2024-01-10 02:53:04 +0700

    feat: save template to local storage

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -4,13 +4,12 @@
 <head>
     <meta charset="utf-8">
     <title>GrapesJS Preset Webpage</title>
-    <link href="{{asset('grapesjs/css/grapes.min.css')}}" rel="stylesheet">
-    <script src="{{asset('grapesjs/js/grapes.min.js')}}"></script>
-    <script src="{{asset('grapesjs/js/grapes-preset-webpage.js')}}"></script>
-    <script src="https://unpkg.com/grapesjs-preset-newsletter"></script>
-    <script src="https://unpkg.com/grapesjs-navbar"></script>
-    <script src="https://unpkg.com/grapesjs-plugin-forms"></script>
-    <script src="https://unpkg.com/grapesjs-image-plugin"></script>
+    <link href="{{ asset('grapesjs/css/grapes.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('grapesjs/js/grapes.min.js') }}"></script>
+    <script src="{{ asset('grapesjs/js/grapesjs-preset-webpage.js') }}"></script>
+    <script src="{{ asset('grapesjs/js/grapesjs-preset-newsletter.js') }}"></script>
+    <script src="{{ asset('grapesjs/js/grapesjs-navbar.js') }}"></script>
+    <script src="{{ asset('grapesjs/js/grapesjs-plugin-forms.js') }}"></script>
     <style>
         body,
         html {
@@ -23,12 +22,14 @@
 <body>
 
     <div id="gjs" style="height:0px; overflow:hidden">
-        {!!$page->content!!}
+        {!! $page->content !!}
     </div>
+    <div id="blocks"></div>
 
 
     <script type="text/javascript">
         window.onload = () => {
+            const projectId = '{{$page->id}}'
             window.editor = grapesjs.init({
                 height: '100%',
                 showOffsets: true,
@@ -36,16 +37,28 @@
                 storageManager: false,
                 container: '#gjs',
                 fromElement: true,
-
                 plugins: [
                     'grapesjs-preset-webpage',
                     'grapesjs-preset-newsletter',
                     'grapesjs-navbar',
-                    'grapesjs-plugin-forms',
-                    'grapesjs-image-plugin'
+                    'grapesjs-plugin-forms'
                 ],
                 pluginsOpts: {
-                    'grapesjs-image-plugin': {}
+
+                },
+                canvas: {
+
+                },
+                storageManager: {
+                    type: 'local', // Type of the storage, available: 'local' | 'remote'
+                    autosave: true, // Store data automatically
+                    autoload: true, // Autoload stored data on init
+                    stepsBeforeSave: 1, // If autosave enabled, indicates how many changes are necessary before store method is triggered
+                    options: {
+                        local: { // Options for the `local` type
+                            key: projectId, // The key for the local storage
+                        },
+                    }
                 }
 
             });
